0737348bb9a9a4be394a49b0b048853c2086da6f
hmd <hmd19570@gmail.com>
2021-11-01 23:33:10 +0100

    Aflevering 10

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<header>
+    <title>My blog</title>
+    <link rel="stylesheet" href="/app.css">
+
+
+</header>
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post </a></h1>
+        <p>Nulla sit incididunt ullamco dolor. Ipsum excepteur laboris laborum cupidatat. Officia duis Lorem veniam
+            tempor voluptate Lorem. Non non sit ipsum adipisicing. Ullamco ut ullamco minim nisi eiusmod cillum quis
+            fugiat ullamco sint. Esse ad eiusmod voluptate culpa officia nulla. Exercitation id Lorem voluptate deserunt
+            proident sunt.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My second post </a></h1>
+        <p>Cupidatat exercitation reprehenderit veniam laboris qui enim ut sint dolore. Eu qui quis velit aliqua culpa
+            tempor ullamco amet enim aute sit. Nulla qui tempor magna anim voluptate elit.</p>
+    </article>
+
+
+    <article>
+        <h1><a href="/posts/my-third-post">My third post </a></h1>
+        <p>Voluptate aute laboris officia qui eu laboris deserunt aliquip nisi. Ullamco ullamco duis et duis eu. Nulla
+            nostrud commodo velit sint nulla reprehenderit ea adipisicing irure in magna. Consectetur ullamco in velit
+            do ullamco sunt in dolore magna velit id. Nostrud ad proident labore occaecat nulla adipisicing exercitation
+            ut eiusmod excepteur quis pariatur duis nisi.</p>
+    </article>
+</body>
