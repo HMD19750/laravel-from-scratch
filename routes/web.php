4f960414bdb0fd81865a0e85cb7a467808cf7d27
hmd <hmd19570@gmail.com>
2021-11-28 15:26:08 +0100

    Aflevering 60

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,14 @@ use App\Models\User;
 
 
 use App\Models\Category;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\NewsletterController;
+use Illuminate\Validation\ValidationException;
 use App\Http\Controllers\PostCommentsController;
 
 /*
@@ -42,27 +45,4 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
-Route::post('newsletter', function () {
-    request()->validate(['email' => ['required', 'email']]);
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us20'
-    ]);
-
-    try {
-        $response = $mailchimp->lists->addListMember("d1a2df2b7e", [
-            'email_address' => request('email'),
-            'status' => 'subscribed'
-        ]);
-    } catch (\Exception $e) {
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'This email could not be added.'
-        ]);
-    }
-
-
-    return redirect('/')->with('success', "You are now subscribed to our newsletter.");
-});
+Route::post('newsletter', NewsletterController::class);
