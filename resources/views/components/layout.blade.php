552448a9a14714eca2f46baccb7c0fa90aa8a09a
hmd <hmd19570@gmail.com>
2021-11-28 14:27:30 +0100

    Aflevering 59

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,12 @@
 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap" rel="stylesheet">
 <script defer src="https://unpkg.com/alpinejs@3.5.1/dist/cdn.min.js"></script>
 
+<style>
+    html {
+        scroll-behavior: smooth;
+    }
+</style>
+
 <body style="font-family: Open Sans,sans-serif">
     <section class="px-6 py-8 ">
         <nav class="flex justify-between items-center">
@@ -21,15 +27,15 @@
                 <a href="/register" class="text-xs font-bold uppercase">Register</a>
                 <a href="/login" class="ml-6 text-xs font-bold uppercase">Log in</a>
                 @else
-                    <span class="text-xs  font-bold uppercase">Welcome {{ auth()->user()->name }}</span>
+                <span class="text-xs  font-bold uppercase">Welcome {{ auth()->user()->name }}</span>
 
-                    <form method="POST" action="/logout" class="text-xs text-blue-500 font-semibold ml-6">
-                        @csrf
-                        <button type="submit">Logout</button>
-                    </form>
+                <form method="POST" action="/logout" class="text-xs text-blue-500 font-semibold ml-6">
+                    @csrf
+                    <button type="submit">Logout</button>
+                </form>
                 @endguest
 
-                <a href="#"
+                <a href="#newsletter"
                     class="px-3 py-3 uppercase bg-blue-500 ml-3 text-xs font-semibold text-white rounded-full">Subscribe
                     for updates</a>
             </div>
@@ -38,7 +44,8 @@
         {{ $slot }}
 
 
-        <footer class="py-16 px-10 mt-15 text-center bg-gray-100 border border-black border-opacity-5 rounded-xl">
+        <footer id="newsletter"
+            class="py-16 px-10 mt-15 text-center bg-gray-100 border border-black border-opacity-5 rounded-xl">
             <img src="/images/lary-newsletter-icon.png" alt="Larry" class="mx-auto" style="width:145px;">
             <h5 class="text-3xl">Stay in touch with the latest posts</h5>
             <p class="text-sm">Promise to keep the mailbox clean. No bugs.</p>
@@ -47,8 +54,18 @@
                 <div class="relative inline-block mx-auto  py-3 px-5 rounded-full bg-gray-200">
                     <img src="/images/mailbox-icon.svg" alt="mailbox">
 
-                    <form method="POST" action="#" class="flex text-sm">
-                        <input type="text" placeholder="Your email address" class="pl-4 bg-transparent">
+                    <form method="POST" action="/newsletter" class="flex text-sm">
+                        @csrf
+                        
+                        <div>
+                            <input type="email" name="email" id="email" placeholder="Your email address"
+                                class="pl-4 bg-transparent">
+
+                            @error('email')
+                                <span class="text-xs text-red-500">{{ $message }}</span>
+                            @enderror
+                        </div>
+
                         <button type="submit"
                             class="px-3 py-3 uppercase bg-blue-500 ml-3 text-xs font-semibold text-white rounded-full">Subscribe</button>
                     </form>
--- routes/web.php
+++ routes/web.php
@@ -41,3 +41,28 @@ Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth'
 
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('newsletter', function () {
+    request()->validate(['email' => ['required', 'email']]);
+
+    $mailchimp = new \MailchimpMarketing\ApiClient();
+
+    $mailchimp->setConfig([
+        'apiKey' => config('services.mailchimp.key'),
+        'server' => 'us20'
+    ]);
+
+    try {
+        $response = $mailchimp->lists->addListMember("d1a2df2b7e", [
+            'email_address' => request('email'),
+            'status' => 'subscribed'
+        ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added.'
+        ]);
+    }
+
+
+    return redirect('/')->with('success', "You are now subscribed to our newsletter.");
+});
